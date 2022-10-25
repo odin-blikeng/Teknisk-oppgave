a4490aea8e73006538ec749ba65c7d9488a7fcd3
Odin Fasteland Blikeng <o.blikeng@stud.uis.no>
2022-10-25 22:51:35 +0200

    house keeping

--- timeforing/App/ClassLib/Project.cs
+++ timeforing/App/ClassLib/Project.cs
@@ -5,13 +5,11 @@ public class Project
     {
         Title = title;
     }
-    //  private Project() { }
 
     public Guid Id { get; protected set; } = Guid.NewGuid();
-    public string Title { get; set;}
+    public string Title { get; set; }
 
-    // TimeSpan TotalHours { get; set; }
-    public List<TimeCard> Hours{ get; set; } =new List<TimeCard>();
+    public List<TimeCard> Hours { get; set; } = new List<TimeCard>();
     public void AddHours(Guid driverId, double hours)
     {
         Hours.Add(new TimeCard(driverId, hours));
