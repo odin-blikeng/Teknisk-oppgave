24a23cb7615faead99af874f8eac9e4277e4b2bc
Odin Fasteland Blikeng <o.blikeng@stud.uis.no>
2022-11-13 17:54:23 +0100

    removed temp files.

--- timeforing/App/Program.cs
+++ timeforing/App/Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddDbContext<TimekeepingContext>(options =>
 // options.UseSqlite($"Data Source={Path.Combine("Infrastructure", "Data", "dataSQLite.db")}"
 options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
-));
+)
+);
 
 builder.Services.AddDistributedMemoryCache();
 
